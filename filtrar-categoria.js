1b43072e86add2ba41b6c3a5085f96ac197a79f5
Lucas Rico <lucasfernandezrico22@gmail.com>
2023-10-26 20:44:44 +0200

    ramas

--- filtrar-categoria.js
+++ filtrar-categoria.js
@@ -1,1 +1,2 @@
-//Recibe un listado de productos y una categoria, devuelv e array con los prodcutos que son de esa categoria.
+//Recibe un listado de productos y una categoria, devuelve
+// array con los prodcutos que son de esa categoria.
