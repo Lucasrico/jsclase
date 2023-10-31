248a1ff952cc0edf38fbe9d4fd42d9e271a313a7
Lucas Rico <lucasfernandezrico22@gmail.com>
2023-10-31 20:34:29 +0100

    mas promesas

--- contiene-negativo-promesa.js
+++ contiene-negativo-promesa.js
@@ -9,10 +9,37 @@
  * Ejemplo 2: para el array [1, 2, -1] debe llamar a reject y mostrar por consola -1
  */
 
+const listado = [8, 4, 9, 12, 32, 9, 58, 14];
+const listadoConNegativo = [8, 4, 9, 12, 32, 9, 58, -14,];
+
+function ok(array) {
+    console.log(array);
+}
+
+function nook(valornegativo) {
+    console.log(valornegativo)
+}
+
 function comprobarNegativo(listado) {
-    let p = new Promise(function(resolve, reject) {
-        // escribe tu codigo aqui
+    let p = new Promise(function (resolve, reject) {
+        let haynegativo = false;
+        let valornegativo = 0;
+        for (let i = 0; i < listado.length; i++) {
+            if (listado[i] < 0) {
+                haynegativo = true;
+                valornegativo = listado[i];
+            }
+        }
+        if (!haynegativo) {
+            resolve(listado);
+        } else {
+            reject(valornegativo);
+        }
+
     });
 
     return p;
 }
+
+comprobarNegativo(listado).then(ok).catch(nook);
+comprobarNegativo(listadoConNegativo).then(ok).catch(nook);
